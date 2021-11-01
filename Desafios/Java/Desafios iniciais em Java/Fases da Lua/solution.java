3e1d36af47efe31da75b369266c1020e1fb71cc8
Ricardo Shikata <rshikata@apoioprodesp.sp.gov.br>
2021-11-01 19:46:57 -0300

    Fix Fases Lua

--- Desafios/Java/Desafios iniciais em Java/Fases da Lua/solution.java
+++ Desafios/Java/Desafios iniciais em Java/Fases da Lua/solution.java
@@ -10,9 +10,9 @@ public class Teste {
         
         if (inicio >= 0 && fim <= 2) {
             System.out.println("nova");
-        } else if (fim > inicio && fim <= 96) {
+        } else if (inicio >= 3 && fim <= 96) {
             System.out.println("minguante"); //implemente o código aqui
-        } else if (inicio >= fim && fim <= 96) {
+        } else if (inicio >= 96 && fim <= 3 ) {
             System.out.println("crescente"); //implemente o código aqui
         } else { 
             System.out.println("cheia");  //implemente o código aqui
