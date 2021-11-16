4a6f2bdfda889aac3b8cab15d598c170b5ad9781
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-11-16 20:17:28 -0300

    readme

--- Desafios/C Sharp/Resolvendo desafios Intermediários em C#/PUM/solucao.cs
+++ Desafios/C Sharp/Resolvendo desafios Intermediários em C#/PUM/solucao.cs
@@ -9,17 +9,14 @@ O arquivo de entrada contém um número inteiro positivo N.
 Saída
 Imprima a saída conforme o exemplo fornecido.
 
- 
 Exemplo de Entrada	Exemplo de Saída
-7
-
-1 2 3 PUM
-5 6 7 PUM
-9 10 11 PUM
-13 14 15 PUM
-17 18 19 PUM
-21 22 23 PUM
-25 26 27 PUM
+7                   1 2 3 PUM
+                    5 6 7 PUM
+                    9 10 11 PUM
+                    13 14 15 PUM
+                    17 18 19 PUM
+                    21 22 23 PUM
+                    25 26 27 PUM
 */
 
 using System;
